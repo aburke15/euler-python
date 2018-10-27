c52ed2ba6697ba6bc97d234f7c81aa79d79bd314
Andre Burke <aburkex24@gmail.com>
2018-10-26 18:19:32 -0600

    added finished problem 2

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,25 @@
 #1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 #By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 #Euler #2
+
+# algo to get fibonacci numbers under 1 mil
+#   then only get the even numbers
+
+count = 0
+first = 0
+next = 0
+sum = 0
+
+while count <= 4_000_000:
+    if count is 0:
+        first = count
+        next = count + 1
+
+    count = first + next
+    if count % 2 is 0:
+        sum += count
+
+    first = next
+    next = count
+
+print(sum)
